c538ee8251141b4de6d5209a94505c730bf51d5a
adesupriadi <135812269+adesupriadi@users.noreply.github.com>
2025-12-01 17:12:57 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// KONFIGURASI BERSIH (STANDARD)
-// Kita menghapus "Plugin Paksa Header" karena kita menggunakan FFmpeg v0.10.1.
-// Versi ini tidak membutuhkan SharedArrayBuffer, jadi lebih aman tanpa header ketat.
-
 export default defineConfig({
   plugins: [react()],
-  // Tambahan opsional: Supaya port-nya konsisten 5173
   server: {
     port: 5173,
-    strictPort: true, 
-  }
+    // Headers ini membantu browser mengizinkan fitur memori di Localhost
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
 })
